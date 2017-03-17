ba5721ad31ca204ece1d97f6bc6c0901c7abeb60
Lisa Fibonacci <lisafibonacci@gmail.com>
2017-03-17 10:15:37 +0200

    .

--- Hidden.php
+++ Hidden.php
@@ -2,8 +2,6 @@
 <title>Let the game begin!</title>
 <body>
 <?php
-//.php?find=abc" accesskey="C" onclick="alalertert(1)
-//windows icin  ALT+SHIFT+C seklinde MACOS icin ctrl+Alt+C seklinde suistimal edilebilir.
 $find=$_GET['find'];
 $block=array(alert,prompt,script,img,textarea,plaintext,svg,iframe);
 $last= str_replace($block, '', $find) ;
